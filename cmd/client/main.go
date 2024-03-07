370e062acce17fee1c7becb564f443c323ceb1e3
Mikołaj Siatkowski <msiatkowski@tutanota.com>
2024-03-08 00:56:44 +0100

    multithreaded client with server exit handling

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -12,8 +12,30 @@ import (
 	"time"
 )
 
-func main() {
+var closed = false
 
+func handleIncoming(conn net.Conn) {
+    b := make([]byte, 4096)
+    for {
+		conn.SetReadDeadline(time.Now().Add(200 * time.Millisecond))
+		n, err := conn.Read(b)
+
+        if err != nil {
+            if netErr, ok := err.(net.Error); !(ok && netErr.Timeout()) {
+                fmt.Println("\nConnection shut down by server")
+                closed = true
+                break
+            }
+        }
+
+		// print new messages if there are any
+		if n > 0 {
+			fmt.Print(string(b[:n]))
+		}
+    }
+}
+
+func main() {
 	conn, err := net.Dial("tcp", "localhost:9001")
 	defer conn.Close()
 
@@ -22,7 +44,7 @@ func main() {
 	}
 
 	// get client id from server
-	b := make([]byte, 4096)
+	b := make([]byte, 64)
 	conn.SetReadDeadline(time.Now().Add(5 * time.Second))
 	_, err = conn.Read(b)
 
@@ -33,35 +55,23 @@ func main() {
 	buffer := bytes.NewReader(b)
 	var id uint64
 	binary.Read(buffer, binary.NativeEndian, &id)
+    fmt.Println("Hello Client ", id)
 
+    go handleIncoming(conn)
 	reader := bufio.NewReader(os.Stdin)
 	for {
-		fmt.Printf("Enter a command: [c]hat\n> ")
-		read, err := reader.ReadBytes('\n')
-
-        if err == io.EOF {
-            fmt.Println("\nexiting...")
+        if closed {
             break
         }
+		read, err := reader.ReadBytes('\n')
 
-        conn.SetReadDeadline(time.Now().Add(200 * time.Millisecond))
-        n, _ := conn.Read(b)
+		if err == io.EOF {
+			fmt.Println("\nexiting...")
+			break
+		}
 
-        // print new messages if there are any
-        if n > 0 {
-            fmt.Print(string(b[:n]))
-        }
-
-        cmd := read[0]
-		switch cmd {
-		case 'c':
-			fmt.Printf("Client %d> ", id)
-			text, _ := reader.ReadString('\n')
-
-			if len(text) > 1 {
-                conn.Write([]byte(text))
-			}
-		default:
+		if len(read) > 1 {
+			conn.Write(read)
 		}
 	}
 }
